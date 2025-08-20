42266f407d34a64d71318518c332c88dc94e6e99
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-21 01:38:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "trm-mz"
+    workspaces {
+      name = "azure-pd-demo"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-postgres-demo"
+  location = "South India"
+}
+
+resource "azurerm_postgresql_server" "pg_server" {
+  name                = "pg-server-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login           = "citus"
+  administrator_login_password  = var.pg_password
+
+  sku_name   = "B_Gen5_1"
+  version    = "11"
+  storage_mb = 5120
+
+  backup_retention_days          = 7
+  geo_redundant_backup_enabled   = false
+  auto_grow_enabled              = true
+  public_network_access_enabled  = true
+  ssl_enforcement_enabled        = true
+}
+
+resource "azurerm_postgresql_database" "pg_database" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.pg_server.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
+variable "client_id" {
+  description = "Azure client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "pg_password" {
+  description = "PostgreSQL admin password"
+  type        = string
+  sensitive   = true
+}
+
+output "postgres_connection_string" {
+  value     = "postgresql://citus:${var.pg_password}@${azurerm_postgresql_server.pg_server.fqdn}:5432/${azurerm_postgresql_database.pg_database.name}"
+  sensitive = true
+}
