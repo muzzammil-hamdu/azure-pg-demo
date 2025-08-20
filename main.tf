d57d3e5337d1603bc0590022a987911394a79575
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-21 03:30:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,6 +67,12 @@ output "postgres_connection_string" {
   sensitive = true
 }
 
+output "postgres_admin_password" {
+  value     = var.pg_password
+  sensitive = false
+}
+
+
 output "postgres_fqdn" {
   value = azurerm_postgresql_flexible_server.pg_server.fqdn
 }
