69c6f1561a4477c1f3c0b10429207432d7abc875
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-21 02:32:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  }
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-postgres-demo"
@@ -26,21 +26,17 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_postgresql_flexible_server" "pg_server" {
-  name                   = "pg-server-mz-demo123" # must be globally unique
+  name                   = "pg-server-mz-demo123"
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
-
   administrator_login    = "citus"
   administrator_password = var.pg_password
-
-  version    = "11"
-  sku_name   = "B_Standard_B1ms"
-  storage_mb = 32768
-
-  backup_retention_days        = 7
+  version                = "11"
+  sku_name               = "B_Standard_B1ms"
+  storage_mb             = 32768
+  backup_retention_days  = 7
   geo_redundant_backup_enabled = false
   zone                         = "1"
-
   public_network_access_enabled = true
 }
 
@@ -56,6 +52,12 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_local" {
   server_id        = azurerm_postgresql_flexible_server.pg_server.id
   start_ip_address = "172.167.147.204"
   end_ip_address   = "172.167.147.204"
+}
+
+variable "pg_password" {
+  description = "PostgreSQL administrator password"
+  type        = string
+  sensitive   = true
 }
 
 output "postgres_connection_string" {
